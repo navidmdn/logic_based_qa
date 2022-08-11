fa735a70746709305b011bf6edfeac23a5ad5888
Navid Madani <navidmdn74@gmail.com>
2022-08-11 15:26:39 -0400

    added logic steps for dev and test translator data loader up until raw data loading

---
+++ nl2log/data_loader.py
@@ -0,0 +1,113 @@
+import os
+import re
+from knowledge_handler.prolog import PrologDA
+from knowledge_handler.kb import MetaQAKB
+
+
+class MetaQADataLoader:
+    def __init__(self, base_path):
+        self.base_path = base_path
+        self.prolog_da = PrologDA()
+
+        kb_path = os.path.join(base_path, 'kb.txt')
+        self.kb = MetaQAKB(kb_path)
+
+        self.prolog_da.add_kb_entities_and_relations(self.kb, add_reverse_rel=True)
+
+        self.logic_to_predicate_dict = {
+            'actor_movie': 'starred_actors_reverse',
+            'director_movie': 'directed_by_reverse',
+            'movie_actor': 'starred_actors',
+            'movie_director': 'directed_by',
+            'movie_genre': 'has_genre',
+            'movie_imdbrating': 'has_imdb_rating',
+            'movie_imdbvotes': 'has_imdb_votes',
+            'movie_language': 'in_language',
+            'movie_tags': 'has_tags',
+            'movie_writer': 'written_by',
+            'movie_year': 'release_year',
+            'tag_movie': 'has_tags_reverse',
+            'writer_movie': 'written_by_reverse'
+        }
+
+    @staticmethod
+    def logics_str_to_steps(logics_str):
+        logics_str = logics_str.replace("_to_", "_")
+        logics_str += "_"
+        steps = []
+        logic_steps = []
+        i = 0
+        prev_und_loc = -1
+        while i < len(logics_str):
+            if logics_str[i] == '_' or i == len(logics_str) - 1:
+                steps.append(logics_str[prev_und_loc + 1:i])
+                prev_und_loc = i
+            i += 1
+        for i in range(len(steps) - 1):
+            logic_steps.append(f"{steps[i]}_{steps[i + 1]}")
+        return logic_steps
+
+    def create_query_string(self, question_str, question_steps, logic_to_predicate_dict, entity_vocab, relation_vocab):
+        # extract question concept
+        question_concept = re.findall(r'\[(.+)\]', question_str)[0]
+        # print(question_concept)
+
+        # define prolog variables
+        prolog_vars = ['X', 'Y', 'Z']
+        logic_steps = self.logics_str_to_steps(question_steps)
+        predicate_steps = [logic_to_predicate_dict[x] for x in logic_steps]
+
+        # create predicates
+        p1 = f'{predicate_steps[0]}({question_concept},X)'
+        p1_query = f'{relation_vocab[predicate_steps[0]]}({entity_vocab[question_concept]},X)'
+        i = 1
+        predicates = [p1, ]
+        predicates_query = [p1_query, ]
+        while i < len(predicate_steps):
+            predicates.append(f"{predicate_steps[i]}({prolog_vars[i - 1]},{prolog_vars[i]})")
+            predicates_query.append(
+                f'{relation_vocab[predicate_steps[i]]}({prolog_vars[i - 1]},{prolog_vars[i]})'
+            )
+            i += 1
+
+        query_string = ', '.join(predicates_query)
+        return predicates, query_string
+
+    def load_raw_data(self, base_path):
+        multi_hop_paths = ['1hop', '2hop', '3hop']
+        train_raw_data = []
+        test_raw_data = []
+        dev_raw_data = []
+
+        for multi_hop_path in multi_hop_paths:
+            hop_path = os.path.join(base_path, multi_hop_path)
+            for split, ds in zip(['train', 'test', 'dev'], [train_raw_data, test_raw_data, dev_raw_data]):
+                print(f"loading {split} dataset")
+
+                questions_path = os.path.join(hop_path, f'qa_{split}.txt')
+                logical_steps_path = os.path.join(hop_path, f'qa_{split}_qtype.txt')
+                questions = []
+                logical_steps = []
+
+                with open(questions_path, 'r') as f:
+                    lines = f.read().strip().split('\n')
+                    for line in lines:
+                        q, a = line.split('\t')
+                        questions.append(q)
+
+                with open(logical_steps_path, 'r') as f:
+                    lines = f.read().strip().split('\n')
+                    assert len(lines) == len(questions)
+
+                    for logic_step, question in zip(lines, questions):
+                        predicates, _ = self.create_query_string(
+                            question,
+                            logic_step,
+                            self.logic_to_predicate_dict,
+                            self.prolog_da.ent_vocab,
+                            self.prolog_da.rel_vocab
+                        )
+                        logical_steps.append(", ".join(predicates))
+                ds.extend(list(zip(questions, logical_steps)))
+        return train_raw_data, test_raw_data, dev_raw_data
+
